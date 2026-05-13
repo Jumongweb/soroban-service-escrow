58810192b02408cd7812fdb72978abef3347c343
macbook <lawaltoheeb36@gmail.com>
2026-05-13 19:08:37 +0100

    Rename hello world contract to service escrow

---
+++ contracts/escrow/contracts/service-escrow/src/lib.rs
@@ -0,0 +1,28 @@
+#![no_std]
+
+use soroban_sdk::{contract, contractimpl, symbol_short, Env, Symbol};
+
+#[contract]
+pub struct ServiceEscrowContract;
+
+#[contractimpl]
+impl ServiceEscrowContract {
+    pub fn hello(env: Env) -> Symbol {
+        symbol_short!("escrow")
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use soroban_sdk::Env;
+
+    #[test]
+    fn test_hello() {
+        let env = Env::default();
+        let contract_id = env.register(ServiceEscrowContract, ());
+        let client = ServiceEscrowContractClient::new(&env, &contract_id);
+
+        assert_eq!(client.hello(), symbol_short!("escrow"));
+    }
+}
